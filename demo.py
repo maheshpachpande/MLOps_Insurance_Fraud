427f60d6c16b6b5eaa7a21e834efe14e1d7a798c
maheshpachpande <pachpandemahesh300@gmail.com>
2025-05-27 12:11:30 +0530

    exception added

--- demo.py
+++ demo.py
@@ -1,6 +1,12 @@
 
 
 from mlops.logger import logging
+from mlops.exception import MLOpsInsuranceException
+import sys
 
-logging.info("This is an info message.")
+#logging.info("This is an info message.")
 
+try:
+    a = 1 / 0
+except Exception as e:
+    raise MLOpsInsuranceException(e, sys) from e
